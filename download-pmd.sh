de2ac53680c8fad0d6bb4649545c0565569378c8
Rodolphe Blancho <rodolphe.blancho@gmail.com>
2019-09-30 17:51:27 +1000

    cache PMD downloads

--- download-pmd.sh
+++ download-pmd.sh
@@ -5,15 +5,19 @@ set -e
 PMD_VERSION=6.18.0
 DOWNLOAD_URL="https://github.com/pmd/pmd/releases/download/pmd_releases%2F${PMD_VERSION}/pmd-bin-${PMD_VERSION}.zip"
 
-printf "Downloading PMD version %s from %s ..." "$PMD_VERSION" "$DOWNLOAD_URL"
+[ -d "./.cache" ] || mkdir ./.cache
+
+if [ -f "./.cache/pmd-bin-${PMD_VERSION}.zip" ]; then
+  printf "Using cached version of PMD at ./.cache/pmd-bin-%s.zip\n" "${PMD_VERSION}"
+else
+  printf "Downloading PMD version %s from %s ...\n" "$PMD_VERSION" "$DOWNLOAD_URL"
+  curl -sL "$DOWNLOAD_URL" -o "./.cache/pmd-bin-${PMD_VERSION}.zip"
+fi
 
 rm -rf ./vendor
 mkdir ./vendor
 
-cd vendor/ || exit 1
-curl -sL "DOWNLOAD_URL" -o "pmd-bin-${PMD_VERSION}.zip"
+unzip "./.cache/pmd-bin-${PMD_VERSION}.zip" -d ./vendor
+mv "./vendor/pmd-bin-${PMD_VERSION}" "./vendor/pmd"
 
-unzip "pmd-bin-${PMD_VERSION}.zip"
-rm -f "pmd-bin-${PMD_VERSION}.zip"
-
-mv "pmd-bin-${PMD_VERSION}" pmd
+exit 0
